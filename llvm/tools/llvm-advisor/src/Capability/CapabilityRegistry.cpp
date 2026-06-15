174eb07b82115df699daf8f3112a915f54a3de25
kamini08 <kaminibanait03@gmail.com>
2026-06-15 17:03:12 +0530

    [llvm-advisor] add hotspot analyzer and capability
    Add RemarksHotspotAnalyzer that aggregates optimization remarks by
    function, file, and line to identify hotspots. Register the
    builtin.remarks_hotspot runner and add the llvm.remarks.hotspot
    capability to the catalog with dependency on llvm.remarks.summary.
    
    Signed-off-by: kamini08 <kaminibanait03@gmail.com>

--- llvm/tools/llvm-advisor/src/Capability/CapabilityRegistry.cpp
+++ llvm/tools/llvm-advisor/src/Capability/CapabilityRegistry.cpp
@@ -38,6 +38,7 @@
 #include "Analysis/IR/PassTimingAnalyzer.h"
 #include "Analysis/IR/RemarksAnalyzer.h"
 #include "Analysis/IR/RemarksMixAnalyzer.h"
+#include "Analysis/IR/RemarksHotspotAnalyzer.h"
 #include "Analysis/IR/RemarksRelationalAnalyzer.h"
 #include "Analysis/IR/RemarksSizeDiffAnalyzer.h"
 #include "Analysis/IR/SimilarityAnalyzer.h"
@@ -319,6 +320,8 @@ void CapabilityRegistry::addBuiltinRunners() {
                          std::make_unique<RemarksSizeDiffAnalyzer>()));
   consumeError(addRunner("builtin.remarks_relational",
                          std::make_unique<RemarksRelationalAnalyzer>()));
+  consumeError(addRunner("builtin.remarks_hotspot",
+                         std::make_unique<RemarksHotspotAnalyzer>()));
 
   // Inspection analyzers
   consumeError(
